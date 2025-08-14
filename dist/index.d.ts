56f25bb99eee3299142e290d3c898ddb1dd8eadd
Hernawan Faiz Abdillah <hernawan.faiz.abdillah@gmail.com>
2025-08-14 07:48:21 +0700

    chore: build

---
+++ dist/index.d.ts
@@ -0,0 +1,18 @@
+export = expressListEndpoints;
+declare function expressListEndpoints(app: import('express').Express | import('express').Router | any): Endpoint[];
+declare namespace expressListEndpoints {
+    export { Route, Endpoint };
+}
+type Endpoint = {
+    path: string;
+    methods: string[];
+    middlewares: {
+        name: string;
+        fn: Function;
+    }[];
+};
+type Route = {
+    methods: Object;
+    path: string | string[];
+    stack: any[];
+};
